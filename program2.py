f81ddd4392e142badb912c55d8f6193b590aecea
Anirudh <anirudhpendem913@gmail.com>
2022-01-17 18:17:20 +0530

    Sample programs using Regular expressions

---
+++ program2.py
@@ -0,0 +1,12 @@
+import re
+
+regx = r"^\d{10}$"
+
+match = re.compile(regx)
+
+PhoneNumber = input("Enter the Phone Number:")
+
+if (re.search(match,PhoneNumber)):
+	print ("Valid Number")	
+else :
+	print ("Invalid Number")
